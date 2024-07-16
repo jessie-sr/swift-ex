42b4b395589b0d72d9c49c0ce569f894ec7d3245
jessie-sr <jessie_sr@163.com>
2024-07-16 22:42:04 +0800

    Formatted doc

--- headers/soa.hpp
+++ headers/soa.hpp
@@ -16,12 +16,11 @@ using namespace std;
  * events on a Service. This listener should be registered on a Service for the Service
  * to notify all listeners for these events.
  */
-template<typename V>
+template <typename V>
 class ServiceListener
 {
 
 public:
-
   // Listener callback to process an add event to the Service
   virtual void ProcessAdd(V &data) = 0;
 
@@ -30,21 +29,19 @@ public:
 
   // Listener callback to process an update event to the Service
   virtual void ProcessUpdate(V &data) = 0;
-
 };
 
 /**
  * Definition of a generic base class Service.
  * Uses key generic type K and value generic type V.
  */
-template<typename K, typename V>
+template <typename K, typename V>
 class Service
 {
 
 public:
-
   // Get data on our service given a key
-  virtual V& GetData(K key) = 0;
+  virtual V &GetData(K key) = 0;
 
   // The callback that a Connector should invoke for any new or updated data
   virtual void OnMessage(V &data) = 0;
@@ -54,9 +51,8 @@ public:
   virtual void AddListener(ServiceListener<V> *listener) = 0;
 
   // Get all listeners on the Service.
-  virtual const vector< ServiceListener<V>* >& GetListeners() const = 0;
-
-};  
+  virtual const vector<ServiceListener<V> *> &GetListeners() const = 0;
+};
 
 /**
  * Definition of a Connector class.
@@ -66,15 +62,13 @@ public:
  * for a publisher Connector.
  * Note that a Connector can be publisher-only, subscriber-only, or both publisher and susbcriber.
  */
-template<typename V>
+template <typename V>
 class Connector
 {
 
 public:
-
   // Publish data to the Connector
   virtual void Publish(V &data) = 0;
-
 };
 
 #endif
